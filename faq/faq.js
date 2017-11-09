be49cb1db8cdba2f4a6f312d3c68c270bcdc0604
Craig Sakuma <craig.sakuma@gmail.com>
2017-11-08 19:08:49 -0800

    added faq exercise and fizzbuzz.js example

---
+++ faq/faq.js
@@ -0,0 +1,24 @@
+$("p").slideUp();
+
+$("#q1").click(toggleAnswer1);
+function toggleAnswer1() {
+	$("#a1").slideToggle();
+}
+
+$("#q2").click(toggleAnswer2);
+
+function toggleAnswer2() {
+	$("#a2").slideToggle();
+}
+
+$("#q3").click(toggleAnswer3);
+
+function toggleAnswer3() {
+	$("#a3").slideToggle();
+}
+
+$("#q4").click(toggleAnswer4);
+
+function toggleAnswer4() {
+	$("#a4").slideToggle();
+}
