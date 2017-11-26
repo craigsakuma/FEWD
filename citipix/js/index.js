d39a404b52d5c8a301ef172dde92faa810255a54
Craig Sakuma <craig.sakuma@gmail.com>
2017-11-25 22:53:45 -0800

    cleaned up comments on citipix hw

--- citipix/js/index.js
+++ citipix/js/index.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
   $('#submit-btn').click(function(event){
     event.preventDefault();
+    // capture city input and reset field
     var city = $('#city-type').val().toLowerCase().trim();
     $('#city-type').val('')
+    // dictionary of classes and city aliases
     var cities = {'nyc':['new york', 'new york city', 'nyc'],
                   'sf':['san francisco','sf','bay area'],
                   'la':['los angeles','la','lax'],
                   'austin':['austin','atx'],
                   'sydney':['sydney','syd']
                   };
+    // iterate through each class and array of aliases; change class if there's a match
     for (var each in cities){
         if (cities[each].indexOf(city)!=-1) {
-            // console.log('url(../images/'+each+'.jpg)')
-            // $('body').attr('background','url(../images/'+each+'.jpg)')
             $('body').removeClass()
             $('body').addClass(each)
       }
