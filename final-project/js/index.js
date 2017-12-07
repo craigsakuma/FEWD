063f4f3b0c53eb98595ead0982a2f562d289dc25
Craig Sakuma <craig.sakuma@gmail.com>
2017-12-06 21:33:32 -0800

    fixed margins and ad unit in responsive relaxr hw

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -7,42 +7,42 @@ $(document).ready(function(){
 
   $(".editor").on("keydown keyup", function(e){
     if (e.keyCode == 32){
-            console.log($(".editor").value)
+            // console.log($(this).val())
 
-    //     var text = $(this).text().replace(/[\s]+/g, " ").trim();
-    //     var word = text.split(" ");
-    //     var newHTML = "";
+        var text = $(this).text().replace(/[\s]+/g, " ").trim();
+        var word = text.split(" ");
+        var newHTML = "";
 
-    //     $.each(word, function(index, value){
-    //         switch(value.toUpperCase()){
-    //             case "SELECT":
-    //             case "FROM":
-    //             case "WHERE":
-    //             case "LIKE":
-    //             case "BETWEEN":
-    //             case "NOT LIKE":
-    //             case "FALSE":
-    //             case "NULL":
-    //             case "FROM":
-    //             case "TRUE":
-    //             case "NOT IN":
-    //                 newHTML += "<span class='statement'>" + value + "&nbsp;</span>";
-    //                 break;
-    //             default: 
-    //                 newHTML += "<span class='other'>" + value + "&nbsp;</span>";
-    //         }
-    //     });
-    //     $(this).html(newHTML);
+        $.each(word, function(index, value){
+            switch(value.toUpperCase()){
+                case "SELECT":
+                case "FROM":
+                case "WHERE":
+                case "LIKE":
+                case "BETWEEN":
+                case "NOT LIKE":
+                case "FALSE":
+                case "NULL":
+                case "FROM":
+                case "TRUE":
+                case "NOT IN":
+                    newHTML += "<span class='statement'>" + value + "&nbsp;</span>";
+                    break;
+                default: 
+                    newHTML += "<span class='other'>" + value + "&nbsp;</span>";
+            }
+        });
+        $(this).html(newHTML);
         
-    //     //// Set cursor postion to end of text
-    //     var child = $(this).children();
-    //     var range = document.createRange();
-    //     var sel = window.getSelection();
-    //     range.setStart(child[child.length - 1], 1);
-    //     range.collapse(true);
-    //     sel.removeAllRanges();
-    //     sel.addRange(range);
-    //     $(this)[0].focus(); 
+        //// Set cursor postion to end of text
+        // var child = $(this).children();
+        // var range = document.createRange();
+        // var sel = window.getSelection();
+        // range.setStart(child[child.length - 1], 1);
+        // range.collapse(true);
+        // sel.removeAllRanges();
+        // sel.addRange(range);
+        // $(this)[0].focus(); 
     }
 })
 })
